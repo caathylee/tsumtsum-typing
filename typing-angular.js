6b6b0c8af3c3f2197dcaaaf21d83041917c7f790
Cathy <caathylee@gmail.com>
2016-05-24 15:19:00 -0700

    Calculates WPM after user finishes typing the entire paragraph.

--- typing-angular.js
+++ typing-angular.js
@@ -1,24 +1,30 @@
 angular.module('app', [])
 .controller('TypingController', function ($scope, $interval) {
   $scope.testParagraph = '"Hello, Nick," said Harry. "Hello, hello," said Nearly Headless Nick, starting and looking round. He wore a dashing, plumed hat on his long curly hair, and a tunic with a ruff, which concealed the fact that his neck was almost completely severed. He was pale as smoke, and Harry could see right through him to the dark sky and torrential rain outside. "You look troubled, young Potter," said Nick, folding a transparent letter as he spoke and tucking it inside his doublet.';
-  $scope.timePassed = 0;
-  $scope.gameTime = $interval(function () {
-    $scope.timePassed++;
-  }, 1000);
+  $scope.totalTime = 0;
+  $scope.timing;
+  $scope.isCalled = false;
   $scope.keepTrackOfGameRound = function () {
-    $scope.gameTime();
-    if($scope.words.length === 0) {
-      clearInterval($scope.gameTime);
+    if (!$scope.isCalled) {
+      $scope.isCalled = true;
+      $scope.gameTimerFunction();
+    } else if ($scope.words.length === 0) {
+      $interval.cancel($scope.gameTime);
+      $scope.calculateWPM();
     }
+  };
+  $scope.gameTimerFunction = function() {
+    $scope.gameTime = $interval(function () {
+      $scope.totalTime++;
+    }, 1000);
   };
   $scope.hideInput = true;
   $scope.hideCountdown = true;
-  $scope.secondsLeft = 3;
   $scope.words = $scope.testParagraph.split(' ');
-  $scope.totalTime;
-  $scope.wpm = function(){
+  $scope.wpm;
+  $scope.calculateWPM = function() {
     var totalWords = $scope.testParagraph.split(' ').length;
-    
+    $scope.wpm = Math.floor((totalWords/$scope.totalTime)*60);  
   };
   $scope.wordLimit = $scope.words[0];
   var typed = angular.element(document.querySelector('p#whatYoureTyping'));
@@ -36,7 +42,8 @@ angular.module('app', [])
       //display the word in red
     }
   };
-  $scope.countdown = () => {
+  $scope.secondsLeft = 3;
+  $scope.loadingGameCountdown = () => {
     var timer = $interval(() => {
       if ($scope.secondsLeft <= 0) {
         clearInterval(timer);
@@ -55,6 +62,6 @@ angular.module('app', [])
   $scope.buttonClicked = function () {
     $scope.hideInput = false;
     $scope.hideCountdown = false;
-    $scope.countdown();
+    $scope.loadingGameCountdown();
   };
 });
