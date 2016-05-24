6aef5bc6ddb6710ab91b58664dd1e2d5a1fc8be6
Cathy <caathylee@gmail.com>
2016-05-24 12:32:43 -0700

    Make timer to count round. Doesn't start at correct time or stop at correct time.

--- typing.js
+++ typing.js
@@ -1,13 +1,25 @@
 angular.module('app', [])
 .controller('TypingController', function ($scope, $interval) {
   $scope.testParagraph = '"Hello, Nick," said Harry. "Hello, hello," said Nearly Headless Nick, starting and looking round. He wore a dashing, plumed hat on his long curly hair, and a tunic with a ruff, which concealed the fact that his neck was almost completely severed. He was pale as smoke, and Harry could see right through him to the dark sky and torrential rain outside. "You look troubled, young Potter," said Nick, folding a transparent letter as he spoke and tucking it inside his doublet.';
+  $scope.timePassed = 0;
+  $scope.gameTime = $interval(function () {
+    $scope.timePassed++;
+  }, 1000);
+  $scope.keepTrackOfGameRound = function () {
+    $scope.gameTime();
+    if($scope.words.length === 0) {
+      clearInterval($scope.gameTime);
+    }
+  };
   $scope.hideInput = true;
   $scope.hideCountdown = true;
   $scope.secondsLeft = 3;
   $scope.words = $scope.testParagraph.split(' ');
-  // $scope.wpm = function(totalTime, totalWords){
-  //   $scope.
-  // };
+  $scope.totalTime;
+  $scope.wpm = function(){
+    var totalWords = $scope.testParagraph.split(' ').length;
+    
+  };
   $scope.wordLimit = $scope.words[0];
   var typed = angular.element(document.querySelector('p#whatYoureTyping'));
   $scope.checkTypeField = function() {
@@ -33,6 +45,7 @@ angular.module('app', [])
         userPlayer.addClass('rotate');
         computerPlayer.addClass('rotate');
         $scope.hideCountdown = true;
+        $scope.keepTrackOfGameRound();
       } else {
         --$scope.secondsLeft;
       }
